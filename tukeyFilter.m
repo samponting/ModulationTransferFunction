493404d1007317dbc3c69d6e1dd600194b01a1a6
Sam <sam@Sams-MacBook-Pro-5.local>
2024-05-14 11:27:26 +0100

    tukey fix

--- tukeyFilter.m
+++ tukeyFilter.m
@@ -6,14 +6,14 @@ function w = tukeyFilter(n,alpha,mid)
 
     mm = max(mid, n-mid);
     n2 = round(mm*2);
-    m = (n2-1);
+    m = n2;
 
 
     w = zeros(n,1);
-    for x = 0:m
-        if x <= (alpha*(m))/2
-            w(x+1) = 0.5*(1-cos((2*pi*x)/(alpha*m)));
-        elseif (alpha*m) < x && x < ((2-alpha)*m)
+    for x = 0:m-1
+        if x <= (alpha*(m-1))/2
+            w(x+1) = 0.5*(1-cos((2*pi*x)/(alpha*(m-1))));
+        elseif (alpha*(m-1))/2 < x && x < ((2-alpha)*(m-1))/2
             w(x+1) = 1;
         end
         w(n2-x) = w(x+1);
